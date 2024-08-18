ad9440102efb147076baeec2e7bebff58fd37e5b
valerijmedvid <vmedvid@gmail.com>
2024-08-18 21:01:45 +0200

    add tailwind into project

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
