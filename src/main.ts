1f1881841195bb63aa92b992868be8f1127e6af3
valerijmedvid <vmedvid@gmail.com>
2024-08-18 20:46:14 +0200

    update dependencies and remove quasar

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
 import { createApp } from 'vue'
 
-import '@quasar/extras/material-icons/material-icons.css'
-import { Quasar } from 'quasar'
-import 'quasar/src/css/index.sass'
-
 import App from './App.vue'
 import router from './router'
 
 const myApp = createApp(App)
 
 myApp.use(router)
-
-myApp.use(Quasar, {
-  plugins: {},
-})
 
 myApp.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    quasar({
-      autoImportComponentCase: 'pascal',
-      sassVariables: 'src/styles/quasar-variables.sass',
-    }),
-  ],
+  plugins: [],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
